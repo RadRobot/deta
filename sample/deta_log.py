bba1da99eb377e554ec4a0bd717087340826799d
Radu Nicolae <ranicolae@gmail.com>
2015-07-19 15:22:21 -0400

    packaged now

--- sample/deta_log.py
+++ sample/deta_log.py
@@ -1,4 +1,4 @@
-import Logger
+from deta import Logger
 
 conf = {
 	'logfile': 'sample.log',
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "deta",
+    version = "0.0.1",
+    author = "Radu Nicolae",
+    author_email = "ranicolae@gmail.com",
+    description = ("Tag based debug printing"),
+    license = "MIT",
+    keywords = "logging configuration json tags tagging",
+    url = "https://github.com/RadRobot/deta",
+    packages=['deta']
+)
